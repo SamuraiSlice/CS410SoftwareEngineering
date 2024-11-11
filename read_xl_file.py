96e9226622e8137578346f1dc8a9654d16188b44
SSingh <91551386+SamuraiSlice@users.noreply.github.com>
2024-11-11 15:45:49 -0500

    Update read_xl_file.py

--- read_xl_file.py
+++ read_xl_file.py
@@ -1,22 +1,22 @@
 import pandas as pd
 
-# Function to read an Excel file
+# Read XL file function
 def read_excel_file(file_path):
     try:
-        # Read the Excel file
+        # Read XL file by getting its file path
         df = pd.read_excel(file_path)
         
-        # Display the first few rows of the dataframe
+        # show 1st few rows of dataframe
         print("First 5 rows of the Excel sheet:")
         print(df.head())
         
-        # Optionally, return the entire dataframe
+        # Opt., ret. entire dataframe
         return df
         
     except Exception as e:
         print(f"Error reading the Excel file: {e}")
 
-# Example usage
+# Ex. usage
 if __name__ == "__main__":
     file_path = 'Paragraph_Assignment_Template.xlsx'  # Path to Excel file
     read_excel_file(file_path)
